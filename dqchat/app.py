cffa66fbe23dd3f45c4b2dfb7aeba02a53c86127
Merlintor <merlinfuchs2001@gmail.com>
2019-08-19 21:24:39 +0200

    minor

--- dqchat/app.py
+++ dqchat/app.py
@@ -1,21 +1,18 @@
-import uuid
+import secrets
 from threading import Thread
 from server import Server
 from client import Client
 
 
 class App:
-    def __int__(self):
-        self.token = uuid.uuid4()
+    def __init__(self, port=31213):
+        self.token = secrets.token_hex()
         self.client = Client(self)
+        self.server = Server(self)
+        self.port = port
 
     def on_message(self, data):
-
-        temp = {
-            "author": "ghhasiojasioj.onion",
-            "token": "asfiohasofoiahsfiohasofoasf",
-            "content": "Hallo"
-        }
+        print(data)
 
     def display_message(self):
         pass
@@ -30,9 +27,8 @@ class App:
             print(text_message)
             self.send_message(text_message)
 
-    def run(self, port=31213):
-        server = Server(self)
-        server_thread = Thread(target=server.run, kwargs={"port": port})
+    def run(self):
+        server_thread = Thread(target=self.server.run, kwargs={"port": self.port})
         server_thread.start()
         self.input()
 

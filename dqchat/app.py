ea659bfc5255af9460ba050cd0824b183b47f4fe
Merlintor <merlinfuchs2001@gmail.com>
2019-08-20 12:16:25 +0200

    Basic cli

--- dqchat/app.py
+++ dqchat/app.py
@@ -1,7 +1,10 @@
 import secrets
 from threading import Thread
+import os
+
 from server import Server
 from client import Client
+from tor import TorController
 
 
 class App:
@@ -9,26 +12,60 @@ class App:
         self.token = secrets.token_hex()
         self.client = Client(self)
         self.server = Server(self)
+        self.tor = TorController()
+
         self.port = port
+        self.user_id = self.tor.get_hostname().replace(".onion", "")
+
+        self.chats = {}
+        self.active_chat = None
 
     def on_message(self, data):
-        print(data)
+        self.chats.setdefault(data["author"], [])
+        self.chats[data["author"]].append({
+            "content": data["content"],
+            "received": True,
+        })
+        print(">", data["content"])
 
-    def display_message(self):
-        pass
+    def send_message(self, content):
+        self.chats.setdefault(self.active_chat, [])
+        self.chats[self.active_chat].append({
+            "content": content,
+            "received": False,
+        })
+        self.client.send_message(self.active_chat, content)
 
-    def send_message(self, text):
-        #TODO Send Message
-        pass
+    def start_chat(self, user_id):
+        self.active_chat = user_id
+        if self.active_chat in self.chats.keys():
+            for message in self.chats[self.active_chat]:
+                print(("> " if message["received"] else "") + message["content"])
 
-    def input(self):
         while True:
-            text_message = input()
-            print(text_message)
-            self.send_message(text_message)
+            text = input()
+            if text.lower() == "/exit":
+                break
+
+            self.send_message(text)
+
+    def clear_terminal(self):
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
+
+    def start_menu(self):
+        while True:
+            self.clear_terminal()
+            user_id = input("Which user do you want to chat with?\n")
+            self.start_chat(user_id.replace(".onion", ""))
 
     def run(self):
+        self.tor.create_service()
+        print(self.tor.get_hostname())
+
         server_thread = Thread(target=self.server.run, kwargs={"port": self.port})
         server_thread.start()
-        self.input()
+        self.start_menu()
 

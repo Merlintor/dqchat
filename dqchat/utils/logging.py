d0707a2848d921345221a3f157be506850782640
Merlintor <merlinfuchs2001@gmail.com>
2019-08-21 13:13:28 +0200

    Change log level

--- dqchat/utils/logging.py
+++ dqchat/utils/logging.py
@@ -1,14 +1,17 @@
 import logging
+import sys
 
 
 log_formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
 logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 fileHandler = logging.FileHandler("dqchat.log")
 fileHandler.setFormatter(log_formatter)
+fileHandler.setLevel(logging.DEBUG)
 logger.addHandler(fileHandler)
 
-consoleHandler = logging.StreamHandler()
+consoleHandler = logging.StreamHandler(sys.stdout)
 consoleHandler.setFormatter(logging.Formatter("%(message)s"))
 consoleHandler.setLevel(logging.INFO)
 logger.addHandler(consoleHandler)
